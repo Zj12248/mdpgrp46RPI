83e65430259ec1e94dd6865606e21222a007b99d
Zj12248 <minezijun@gmail.com>
2024-09-18 00:58:35 +0800

    Update rpiSTM32Test.py

--- Testing Scripts/rpiSTM32Test.py
+++ Testing Scripts/rpiSTM32Test.py
@@ -1,4 +1,8 @@
-from communication.stm32 import STMLink  # Replace with your actual module name
+from communication.stm32 import STMLink 
+# STM32 BOARD SERIAL CONNECTION
+# to get connected serial devices: ls /dev/serial/by-id/
+# SERIAL_PORT = "/dev/serial/by-id/usb-Silicon_Labs_CP2102_USB_to_UART_Bridge_Controller_0002-if00-port0"  # stm32
+# BAUD_RATE = 115200
 
 def main():
     # Initialize the STMLink object
